8daa234218675b499312b441aa2b1fe0dcdab15e
pdamianik <39028343+pdamianik@users.noreply.github.com>
2022-11-09 15:33:35 +0100

    add github-action-input-parser

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as parser from 'github-action-input-parser';
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const ms = parser.getInput({ input: 'milliseconds', type: Number, required: true });
+    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(new Date().toTimeString());
+    await wait(ms);
+    core.debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
